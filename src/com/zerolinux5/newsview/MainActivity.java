fcaf6ddf1dee0498b4b1bd238dc14cfe8be90061
Jesus Magana <maganazero5@gmail.com>
2014-05-29 12:12:21 -0700

    Starting list adapter code

--- src/com/zerolinux5/newsview/MainActivity.java
+++ src/com/zerolinux5/newsview/MainActivity.java
@@ -1,42 +1,120 @@
 package com.zerolinux5.newsview;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+	
+	private static final String LOG_TAG = "MainActivity";
 
+	private class ListElement {
+		ListElement() {};
+		
+		public String textLabel;
+		public String buttonLabel;
+	}
+	
+	private ArrayList<ListElement> aList;
+	
+	private class MyAdapter extends ArrayAdapter<ListElement>{
+
+		int resource;
+		Context context;
+		
+		public MyAdapter(Context _context, int _resource, List<ListElement> items) {
+			super(_context, _resource, items);
+			resource = _resource;
+			context = _context;
+			this.context = _context;
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			LinearLayout newView;
+			
+			ListElement w = getItem(position);
+			
+			// Inflate a new view if necessary.
+			if (convertView == null) {
+				newView = new LinearLayout(getContext());
+				String inflater = Context.LAYOUT_INFLATER_SERVICE;
+				LayoutInflater vi = (LayoutInflater) getContext().getSystemService(inflater);
+				vi.inflate(resource,  newView, true);
+			} else {
+				newView = (LinearLayout) convertView;
+			}
+			
+			// Fills in the view.
+			TextView tv = (TextView) newView.findViewById(R.id.listText);
+			Button b = (Button) newView.findViewById(R.id.listButton);
+			tv.setText(w.textLabel);
+			b.setText(w.buttonLabel);
+
+			// Sets a listener for the button, and a tag for the button as well.
+			b.setTag(Integer.toString(position));
+			b.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					// Reacts to a button press.
+					// Gets the integer tag of the button.
+					String s = (String) v.getTag();
+					int pos = Integer.parseInt(s);
+					aList.remove(pos);
+					aa.notifyDataSetChanged();
+				}
+			});
+
+			return newView;
+		}		
+	}
+
+	private MyAdapter aa;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		aList = new ArrayList<ListElement>();
+		aa = new MyAdapter(this, R.layout.list_element, aList);
+		ListView myListView = (ListView) findViewById(R.id.listView1);
+		myListView.setAdapter(aa);
+		aa.notifyDataSetChanged();
 	}
-
+	
+	public void enterItem(View v) {
+		Log.d(LOG_TAG, "The button has been pressed");
+		EditText et = (EditText) findViewById(R.id.editText1);
+		String s = et.getText().toString();
+		ListElement el = new ListElement();
+		el.textLabel = s;
+		el.buttonLabel = "b_" + s;
+		aList.add(el);
+		Log.d(LOG_TAG, "The length of the list now is " + aList.size());
+		aa.notifyDataSetChanged();
+		// Clears the entry text.
+		et.setText("");
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
 }

e11e222f9e9fc16c2c38d1484eecd5db692a6eab
Jesus Magana <maganazero5@gmail.com>
2014-05-29 12:31:03 -0700

    Added json downloader code

--- src/com/zerolinux5/newsview/MainActivity.java
+++ src/com/zerolinux5/newsview/MainActivity.java
@@ -1,10 +1,23 @@
 package com.zerolinux5.newsview;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,11 +30,117 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
 	private static final String LOG_TAG = "MainActivity";
+	private static final int MAX_SETUP_DOWNLOAD_TRIES = 3;
+	
+	// Change this.
+	private static final String DOWNLOAD_URL = "http://luca-ucsc.appspot.com/jsonnews/default/news_sources.json";
+	
+	// Background downloader.
+	private BackgroundDownloader downloader = null;
+	
+	// This function can be associated e.g. with a button.
+	public void startDownload(View v) {
+		downloader = new BackgroundDownloader();
+		downloader.execute(DOWNLOAD_URL);
+	}
+	
+    // This class downloads from the net the camera setup instructions.
+    private class BackgroundDownloader extends AsyncTask<String, String, String> {
+    	
+    	protected String doInBackground(String... urls) {
+    		Log.d(LOG_TAG, "Starting the download.");
+    		String downloadedString = null;
+    		String urlString = urls[0];
+    		URI url = URI.create(urlString);
+    		int numTries = 0;
+    		while (downloadedString == null && numTries < MAX_SETUP_DOWNLOAD_TRIES && !isCancelled()) {
+    			numTries++;
+    			HttpGet request = new HttpGet(url);
+    			DefaultHttpClient httpClient = new DefaultHttpClient();
+    			HttpResponse response = null;
+    			try {
+    				response = httpClient.execute(request);
+    			} catch (ClientProtocolException ex) {
+    				Log.e(LOG_TAG, ex.toString());
+    			} catch (IOException ex) {
+    				Log.e(LOG_TAG, ex.toString());
+    			}
+    			if (response != null) {
+    				// Checks the status code.
+    				int statusCode = response.getStatusLine().getStatusCode();
+    				Log.d(LOG_TAG, "Status code: " + statusCode);
+
+    				if (statusCode == HttpURLConnection.HTTP_OK) {
+    					// Correct response. Reads the real result.
+    					// Extracts the string content of the response.
+    					HttpEntity entity = response.getEntity();
+    					InputStream iStream = null;
+    					try {
+    						iStream = entity.getContent();
+    					} catch (IOException ex) {
+    						Log.e(LOG_TAG, ex.toString());
+    					}
+    					if (iStream != null) {
+    						downloadedString = ConvertStreamToString(iStream);
+    						Log.d(LOG_TAG, "Received string: " + downloadedString);
+    				    	return downloadedString;
+    					}
+    				}
+    			}
+    		}
+    		// Returns the instructions, if any.
+    		return downloadedString;
+    	}
+    	
+    	protected void onPostExecute(String s) {
+    		// This is executed in the UI thread.
+    		//TextView tv = (TextView) findViewById(R.id.textView1);
+    		//tv.setText(s);
+    		//tv.setVisibility(View.VISIBLE);
+    	}
+    	
+    }
+    
+    
+    
+    @Override
+    // This stops the downloader as soon as possible.
+    public void onStop() {
+    	if (downloader != null) {
+    		downloader.cancel(false);
+    	}
+    	super.onStop();
+    }
+    
+    public static String ConvertStreamToString(InputStream is) {
+    	
+    	if (is == null) {
+    		return null;
+    	}
+    	
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+	    StringBuilder sb = new StringBuilder();
+
+	    String line = null;
+	    try {
+	        while ((line = reader.readLine()) != null) {
+	            sb.append((line + "\n"));
+	        }
+	    } catch (IOException e) {
+	        Log.d(LOG_TAG, e.toString());
+	    } finally {
+	        try {
+	            is.close();
+	        } catch (IOException e) {
+	            Log.d(LOG_TAG, e.toString());
+	        }
+	    }
+	    return sb.toString();
+	}
 
 	private class ListElement {
 		ListElement() {};
@@ -109,6 +228,7 @@ public class MainActivity extends Activity {
 		aa.notifyDataSetChanged();
 		// Clears the entry text.
 		et.setText("");
+		startDownload(v);
 	}
 	
 	@Override
